8365e1acb1d42798333eae83e2c8634fe3117ac2
Sfelteau <simonfelteau@gmail.com>
2016-10-31 22:15:51 -0400

    Completion d'interface & Commencé la reception de pourcentage

--- td2/main.js
+++ td2/main.js
@@ -1,18 +1,27 @@
-$(function() {
+$(function start() {
 
     var tokenCounter = new Worker('worker.js');
+	
     tokenCounter.onmessage = function(e) {
+		
+		var pourcent = document.getElementById("pourcent");
+		pourcent.style.width = e.data;
+		
+		
         console.log("Got message from worker: ", e.data);
     };
-    tokenCounter.postMessage({action: "annuler"});
-    tokenCounter.postMessage({action: "compter",
-                              texte: "asdfasdfa"});
+    /*tokenCounter.postMessage({action: "annuler"});
+    tokenCounter.postMessage({action: "compter"});
 
     $("#compter").click(function(e) {
-        console.log("Compter pressed", $("#text").val());
+        console.log("Compter pressed",);
     });
 
     $("#annuler").click(function(e) {
         console.log("Annuler pressed");
-    });
+    });*/
+}
+
+function stop() {
+	tokenCounter.terminate();
 });
--- td2/worker.js
+++ td2/worker.js
@@ -2,9 +2,15 @@
 self.importScripts("CompteurJetons.js");
 var compteur = new CompteurJetons();
 
+
+
 self.addEventListener('message', function(e) {
 
     // Code à implémenter
+	
+	//Je ne suis pas sur quoi faire ici
+	
+	
     console.log("Got message: ", e.data);
     postMessage(e.data);
 
