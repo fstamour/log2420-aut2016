9027fe9717ecc1df0d62b99c50cfe7ec3b8332c8
Francis St-Amour <Francis St-Amour>
2016-11-01 22:37:37 -0400

    Fixed javascript syntax error and button's onClic

--- td2/main.js
+++ td2/main.js
@@ -1,27 +1,23 @@
-$(function start() {
+$(function () {
 
     var tokenCounter = new Worker('worker.js');
-	
+    var progressBar = $("#pourcent");
+
     tokenCounter.onmessage = function(e) {
-		
-		var pourcent = document.getElementById("pourcent");
-		pourcent.style.width = e.data;
-		
-		
+        pourcent.style.width = e.data;
         console.log("Got message from worker: ", e.data);
     };
-    /*tokenCounter.postMessage({action: "annuler"});
-    tokenCounter.postMessage({action: "compter"});
+
+
+    //tokenCounter.postMessage({action: "annuler"});
+    //tokenCounter.postMessage({action: "compter"});
 
     $("#compter").click(function(e) {
-        console.log("Compter pressed",);
+        console.log("Compter pressed");
     });
 
     $("#annuler").click(function(e) {
         console.log("Annuler pressed");
-    });*/
-}
-
-function stop() {
-	tokenCounter.terminate();
+        //tokenCounter.terminate();
+    });
 });
